2d87ddbd13dd6efb47b3bb2e3f9d8e73b52a2ee7
greskelll <jonattan44@hotmail.fr>
2023-01-03 15:44:54 +0100

    premiere ebauche filtres fonctionnel

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,27 +5,39 @@ fetch(`http://localhost:5678/api/works`)
 		}
 	})
 	.then(function (value) {
-		console.log(value);
 		for (let i = 0; i < value.length; i++) {
 			const newProject = document.createElement('figure');
 			newProject.innerHTML = `<img crossorigin="anonymous" src="${value[i].imageUrl}" alt="${value[i].title}">
 				<figcaption>${value[i].title}</figcaption>`;
-			console.log(newProject);
-
 			const gallery = document.getElementsByClassName('gallery').item(0);
-
-			console.log(gallery);
 			gallery.appendChild(newProject);
 		}
-		return fetch(`http://localhost:5678/api/categories`);
+
+		return fetch(`http://localhost:5678/api/works`);
 	})
-	.then(function (cat) {
-		if (cat.ok) {
-			return cat.json();
+	.then(function (res) {
+		if (res.ok) {
+			return res.json();
 		}
 	})
-	.then(function (categories) {
-		console.log(categories);
+	.then(function (res) {
+		let newArray = [];
+		for (i in res) {
+			newArray.push(res[i].categoryId);
+		}
+		console.log(newArray);
+		let figure = document.querySelectorAll('.gallery figure');
+		console.log(figure);
+
+		const objets = document.getElementById('objets');
+		console.log(objets);
+		objets.addEventListener('click', function () {
+			for (let i = 0; i < newArray.length; i++) {
+				if (newArray[i] == 1) {
+					figure[i].remove();
+				}
+			}
+		});
 	})
 
 	.catch(function (err) {
